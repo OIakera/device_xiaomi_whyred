2fa26cbc005e1e660ac0adf122f453210ef0fa89
akirasupr <vishal.rockstar7011@gmail.com>
2022-07-17 17:58:06 +0530

    whyred: PixelOS

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ PRODUCT_COMPRESSED_APEX := false
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
